e3fafd98de23877847cba61842ec7988e2b2d5df
myseocompany <nicolas@myseocompany.co>
2025-02-04 01:21:00 -0500

    test

--- resources/views/components/chat/footer.blade.php
+++ resources/views/components/chat/footer.blade.php
@@ -475,7 +475,7 @@
                 <button  x-show="!((body?.trim()?.length>0) || $wire.media.length > 0 || $wire.files.length > 0 )"
                     wire:loading.attr="disabled" wire:click='sendLike()' type="button" class="group disabled:cursor-progress">
 
-                     <!-- outlined heart -->
+                     <!-- outlined record audio -->
                      <span class=" group-hover:hidden transition">
                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                              stroke="currentColor"
@@ -484,7 +484,7 @@
                                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                          </svg>
                      </span>
-                     <!--  filled heart -->
+                     <!--  filled record audio -->
                      <span class="hidden group-hover:block transition " x-bounce>
                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
                              class="size-6 w-7 h-7   text-red-500">
